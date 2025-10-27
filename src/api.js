d0bc2cd053c0d650a4b8db1a32f2bedd35bbffb5
yukotanjung <yukotanjung@gmail.com>
2025-10-27 09:15:06 +0700

    set true with credential

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
